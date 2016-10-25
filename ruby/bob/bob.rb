2a8a5accfa40ffd37bcf734e59061fe6470f7c50
cck <edouard@cockenpot.net>
2016-10-25 14:20:08 +0200

    bob : updated

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,13 +1,26 @@
 class Bob
   def hey(remark)
-    if remark.split.empty?
-      'Fine. Be that way!'
-    elsif  remark.upcase == remark && !remark.scan(/[A-Z]*/).join.empty?
-      return 'Whoa, chill out!'
-    elsif remark.split("").last == "?"
-      return 'Sure.'
-    else
-      'Whatever.'
+    response ={
+        asking: 'Sure.',
+        yelling: 'Whoa, chill out!',
+        silence: 'Fine. Be that way!',
+        statement: 'Whatever.'
+    }
+
+    #lambdas
+    silence = lambda {|s| s.split.empty?}
+    yelling = lambda {|s| s == s.upcase && s =~ /[A-Z]/}
+    asking  = lambda {|s| s =~ /[?]\z/ }
+
+
+    case remark
+      when silence then  response[:silence]
+      when yelling then response[:yelling]
+      when asking  then response[:asking]
+      else
+        response[:statement]
     end
   end
+
+
 end
